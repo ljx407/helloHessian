e68b9ce1438294d9b7edfc53e8bfe6196a2eafd1
jasonliuhome <ljx407@163.com>
2016-06-14 00:22:45 +0800

    complete

---
+++ helloHessianWithSpringClient/src/main/java/com/ljx/client/HelloWithSpringClient.java
@@ -0,0 +1,15 @@
+package com.ljx.client;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.ljx.service.IHello;
+
+public class HelloWithSpringClient {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring-client.xml");
+		IHello hello = (IHello) context.getBean("helloService");
+		String greetingMessage = hello.sayHello("Alpha");
+		System.out.println("The greeting message is : " + greetingMessage);
+	}
+}
